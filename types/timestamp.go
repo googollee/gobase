fdf28c2250a0f4a0c2fa9ae8620bbcf5cb5dd856
Googol Lee <i@googol.im>
2022-07-14 09:58:01 +0200

    Add helpers.

--- types/timestamp.go
+++ types/timestamp.go
@@ -5,6 +5,14 @@ import "time"
 // Timestamp uses to store a timestamp in database.
 type Timestamp int64
 
+func TimestampNow() Timestamp {
+	return ToTimestamp(time.Now())
+}
+
+func ToTimestamp(t time.Time) Timestamp {
+	return Timestamp(t.Unix())
+}
+
 func (t Timestamp) UTCTime() time.Time {
 	return t.LocalTime().UTC()
 }
